5a45615d3ae72c02a5d67c1509bf8c354b3e6983
zzyybs <zzyybs@126.com>
2016-08-01 16:47:52 +0800

    2
    2

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -7,5 +7,6 @@ public class Test01
 		System.out.println("hello,github服务器");
 		System.out.println("hello,github服务器02");
 		System.out.println("github 回复01");
+		System.out.println("github 回复02");
 	}
 }
